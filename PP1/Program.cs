25d2c2004b72c56df8ab95001d2276247f8f2396
Jose-Maria-Gutierrez <gutierrezjosemaria999@gmail.com>
2022-05-02 16:05:56 -0300

    Agregar archivos de proyecto.

---
+++ PP1/Program.cs
@@ -0,0 +1,18 @@
+﻿int fila, columna;
+fila = columna = 3;
+int[,] matriz = new int[3,3]{ { 200, 30, 3 } , { 33, 54, 51 } , { 32, 32, 2 } };
+Console.WriteLine("el maximo elemento de la matriz es: " + maxMatriz(matriz,fila,columna));
+
+int maxMatriz(int [,]matriz,int f,int c)
+{
+    int i, j;
+    int max = 0;
+    for (i=0;i<f;i++)
+    {
+        for (j=0;j<c;j++)
+        {
+            max = (matriz[i, j] > max) ? matriz[i, j] : max;
+        }
+    }
+    return max;
+}

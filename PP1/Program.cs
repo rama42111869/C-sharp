806f577a80b3cf47b592786727c4da370c220566
Jose-Maria-Gutierrez <gutierrezjosemaria999@gmail.com>
2022-05-02 16:10:52 -0300

    agrego funcion promedioMatriz

--- PP1/Program.cs
+++ PP1/Program.cs
@@ -2,6 +2,7 @@
 fila = columna = 3;
 int[,] matriz = new int[3,3]{ { 200, 30, 3 } , { 33, 54, 51 } , { 32, 32, 2 } };
 Console.WriteLine("el maximo elemento de la matriz es: " + maxMatriz(matriz,fila,columna));
+Console.WriteLine("el promedio de la matriz es: " + promedioMatriz(matriz, fila, columna));
 
 int maxMatriz(int [,]matriz,int f,int c)
 {
@@ -15,4 +16,18 @@ int maxMatriz(int [,]matriz,int f,int c)
         }
     }
     return max;
+}
+
+double promedioMatriz(int[,] matriz, int f, int c)
+{
+    int i, j, acum;
+    acum = 0;
+    for (i = 0; i < f; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            acum += matriz[i, j];  
+        }
+    }
+    return (double)acum / (f*c);
 }

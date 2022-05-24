6b0c899bf1f984557f75dbffd47f5d139bbaa3ad
Rama <45132664+rama42111869@users.noreply.github.com>
2022-05-24 09:48:28 -0300

    Add files via upload

---
+++ Dia13c/Data/Context.cs
@@ -0,0 +1,12 @@
+﻿namespace Dia13c.Data
+{
+    public class Context
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Categoria { get; set; }
+
+        public DateTime Fechabaja { get; set; }
+    }
+}

195550ec6472fce988569c4bb64aedf65a17e650
Rama <45132664+rama42111869@users.noreply.github.com>
2022-06-14 09:23:19 -0300

    Add files via upload

---
+++ LibrosCruz/Models/Libro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibrosCruz.Models
+{
+    public partial class Libro
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Autor { get; set; }
+        public DateTime? Fecha { get; set; }
+    }
+}
